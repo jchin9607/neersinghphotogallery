f4fe9a27c8477ab0f774e223e56da52e8b58897e
jchin9607 <63731736+jchin9607@users.noreply.github.com>
2024-02-27 19:22:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const containerId = 'gallery';
 
-const amountOfImages = 17; //change this if adding new images
+//change these if adding new images
+const amountOfImages = 17; 
+
+const natureImages = 5;
+//
 
 const container = document.getElementById(containerId);
 
 let htmlContent = '';
 
 for (let i = 1; i <= amountOfImages; i++) {
-  htmlContent += `<div class="gallery-item"><img src="https://ik.imagekit.io/64c35uuyg/Portfolio/tr:w-600/${i}.jpg" alt=""></div>`;
+  htmlContent += `<div class="gallery-item misc" data-category="misc"><img src="https://ik.imagekit.io/64c35uuyg/Portfolio/tr:w-600/${i}.jpg" alt="" ></div>`;
+}
+
+for (let i = 1; i <= natureImages; i++) {
+  htmlContent += `<div class="gallery-item nature" data-category="nature"><img src="https://ik.imagekit.io/64c35uuyg/Nature/tr:w-600/${i}.jpg" alt="" ></div>`;
 }
 
 container.innerHTML = htmlContent;
-msnry.reloadItems();
+
+
+
